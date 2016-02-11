3ce7d2bce3cec511228e545d4f293479fa2ed4e7
lucasfradus <lucas.fradusco@gmail.com>
2016-02-11 10:26:10 -0300

    Create pagoC.php

---
+++ pagoC.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+<?php	
+	$dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = '';
+	$dni = $_GET['dni']; 
+	$fecha = $_GET['fecha']; 
+	$concepto = $_GET['concepto']; 
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Error al agregar cliente: ' . mysql_error());
+   }
+   $sql = "INSERT INTO movimientos (dni, fecha, concepto)
+    VALUES ('$dni', '$fecha', '$concepto')";
+echo "pago aplicado con éxito <br>";
+echo "DNI: $dni <br>";
+echo "Concepto: $concepto <br>";
+echo "Fecha de Pago: $fecha <br>";
+
+        mysql_select_db('clientes');
+      $retval = mysql_query( $sql, $conn );  
+   if(! $retval ) {
+      die('Could not enter data: ' . mysql_error());
+   }
+	
+?>
+
+<input type="button" class="btn" onClick="location.href='index.php'" value="Volver al Inicio"/>
+
+ 
