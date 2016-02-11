926fbaeae0edb6e5dc30562e538d151159aced49
lucasfradus <lucas.fradusco@gmail.com>
2016-02-11 10:23:49 -0300

    Create checkin.php

---
+++ checkin.php
@@ -0,0 +1,18 @@
+<HEAD>
+ <TITLE>Check-In</TITLE>
+ <link rel="stylesheet" type="text/css" href="mystyle.css">
+<img src="haya.jpg"  alt="HAYABUSA" align="right">
+<html> 
+<body> 
+<div align="center"> 
+<form name="formularioDatos" method="get" action="check.php">
+<br/><br/><br/><br/>
+
+Ingrese su DNI<br/><br/><input class="textbox" type="int" name="dni" maxlength="8" minlength="8" required/ style="width: 250px;height:75px;font-size:26pt;"><br/><br/>
+<input value="Confirmar" class="btn" type="submit" />
+
+
+</form>
+</div> 
+</body> 
+</html> 
