2ea0f7012a642686c8e6a78a92a01ca60f073768
lucasfradus <lucas.fradusco@gmail.com>
2016-02-11 10:09:26 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<head>
+<title>Menu</title>
+<meta charset="utf-8">
+</head>
+<body>
+<form>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+<img src="haya.jpg"  alt="HAYABUSA" align="right">
+
+<h2> Clientes </h2>
+    <input type="button" class="btn" onClick="location.href='alta.php'" value="Alta nuevo Cliente"/>
+    <input type="button" class="btn" onClick="location.href='busco.php'" value="Modificar Cliente" />
+	<input type="button" class="btn" onClick="location.href='alta.php'" value="Baja Cliente"/>
+    <input type="button" class="btn" onClick="location.href='busco.php'" value="Informar Pago" />
+	<hr>
+ <h2> Reportes </h2>
+    <input type="button" class="btn" onClick="location.href='alta.php'" value="Ver Mensual"/>
+    <input type="button" class="btn" onClick="location.href='busco.php'" value="Ver Por Hora" />
+	<hr>
+<h2> Varios </h2>
+    <input type="button" class="btn" onClick="location.href='img.php'" value="ABM Productos"/>
+    <input type="button" class="btn" onClick="location.href='busco.php'" value="Caja Diaria" />
+	<hr>
+	<br/><br/><br/>
+  <input type="button" class="btn" onClick="location.href='checkin.php'" value="Check In"/>
+
+</div>
+</div>
+</form> 
+</body> 
+</html>
